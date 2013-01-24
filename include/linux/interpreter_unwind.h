622504acbf3fb1059e25ad5cdcb9cefcd2fe780b
Hayawardh Vijayakumar <hvijay@cse.psu.edu>
2013-01-23 19:47:34 -0500

    sting: move interpreter_unwind.h to include/linux

--- include/linux/interpreter_unwind.h
+++ include/linux/interpreter_unwind.h
@@ -8,13 +8,19 @@
  * published by the Free Software Foundation.
  */
 
+#ifndef _INTERPRETER_UNWIND_H
+#define _INTERPRETER_UNWIND_H
+
+#include <linux/user_unwind.h>
+#include <linux/sched.h>
+
 static inline char *int_ept_filename_get(struct user_stack_info *us)
 {
 	return (us->int_trace.nr_entries > 0) ?
-		(us->int_trace.int_filename[0]) : NULL;
+		(us->int_trace.int_filename[0]) : "(null)";
 }
 
-static inline int int_ept_lineno_get(struct user_stack_info *us)
+static inline unsigned long int_ept_lineno_get(struct user_stack_info *us)
 {
 	return (us->int_trace.nr_entries > 0) ? (us->int_trace.entries[0]) : 0;
 }
@@ -25,3 +31,8 @@ static inline int int_ept_exists(struct user_stack_info *us)
 }
 
 extern int is_interpreter(struct task_struct *t);
+extern void copy_interpreter_info(struct task_struct *c, struct task_struct *p);
+extern struct int_bt_info *on_script_behalf(struct user_stack_info *us);
+extern int user_interpreter_unwind(struct user_stack_info *us);
+
+#endif
